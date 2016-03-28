86f5c077ade08d34abf9f0923ac2e900a06f4c7e
Artem <as.pecherkin@gmail.com>
2016-03-28 23:04:31 +0300

    solution for challenge 202 Stepwise Word.

--- Go/202_StepwiseWord/main.go
+++ Go/202_StepwiseWord/main.go
@@ -4,6 +4,29 @@ import "fmt"
 import "log"
 import "bufio"
 import "os"
+import "strings"
+
+func maxLenWord(words []string) (longest string) {
+	max := 0
+	longest = words[0]
+	for k := range words {
+		if len(words[k]) > max {
+			max, longest = len(words[k]), words[k]
+		}
+	}
+	return
+}
+
+func stepwiseWord(word string) (result string) {
+	for k := 0; k < len(word); k++ {
+		tmp := ""
+		for i := 0; i < k; i++ {
+			tmp = "*" + tmp
+		}
+		result = result + tmp + string(word[k]) + " "
+	}
+	return
+}
 
 func main() {
 	file, err := os.Open(os.Args[1])
@@ -13,6 +36,8 @@ func main() {
 	defer file.Close()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		fmt.Println(scanner.Text())
+		word := maxLenWord(strings.Split(scanner.Text(), " "))
+		result := stepwiseWord(word)
+		fmt.Println(result[:len(result)-1])
 	}
 }
